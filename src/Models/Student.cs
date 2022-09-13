491efca82d97222b1cb04b15ef24902ee2a40c40
Bárdos István <bardos.istvan@icloud.com>
2022-09-14 01:44:23 +0200

    Update Student.cs

--- src/Models/Student.cs
+++ src/Models/Student.cs
@@ -1,7 +1,17 @@
+using HogwartsHouses.Models.Types;
+
 namespace HogwartsHouses.Models
 {
     [System.Serializable]
     public class Student
     {
+        public string Name { get; }
+        public PetType? Pet { get; set; }
+
+        public Student(string name, PetType? pet)
+        {
+            Name = name;
+            Pet = pet;
+        }
     }
 }
