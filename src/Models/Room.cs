bb70128b5e6a4c83f95eada727e173a81754afbf
Bárdos István <bardos.istvan@icloud.com>
2022-09-14 01:43:34 +0200

    Update Room.cs

--- src/Models/Room.cs
+++ src/Models/Room.cs
@@ -1,7 +1,22 @@
+using System.Collections.Generic;
+
 namespace HogwartsHouses.Models
 {
     [System.Serializable]
     public class Room
     {
+        private static int _lastId = -1;
+        public int Id { get; }
+        public HashSet<Student> Residents { get; }
+        public bool IsOccupied => Residents.Count == 2;
+
+
+        public Room()
+        {
+            Id = _lastId++;
+            _lastId++;
+            Residents = new HashSet<Student>();
+        }
+
     }
 }
