d82fbb14958faa2f4b1a80cf9c9f65d6ca0f2a51
Bárdos István <bardos.istvan@icloud.com>
2022-09-14 20:38:21 +0200

    Update ApiRoomsController routing
    This is necessary due to the normal RoomsController routing, as it is occupied already.

--- src/Controllers/ApiRoomsController.cs
+++ src/Controllers/ApiRoomsController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HogwartsHouses.Controllers;
 
-[ApiController, Route("[controller]")]
+[ApiController, Route("api/rooms")]
 public class ApiRoomsController : ControllerBase
 {
     private IRoomService _roomService;
