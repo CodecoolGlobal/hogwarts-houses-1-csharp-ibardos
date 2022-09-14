754767a1043b0786ca5a55c8ab8e09ac1574aa18
Bárdos István <bardos.istvan@icloud.com>
2022-09-14 16:35:27 +0200

    Add GetAllRooms() specific method to IRoomService

--- src/Services/Interfaces/IRoomService.cs
+++ src/Services/Interfaces/IRoomService.cs
@@ -1,6 +1,10 @@
+using System.Collections.Generic;
+using HogwartsHouses.Models;
+
 namespace HogwartsHouses.Services.Interfaces
 {
     public interface IRoomService
     {
+        HashSet<Room> GetAllRooms();
     }
 }
