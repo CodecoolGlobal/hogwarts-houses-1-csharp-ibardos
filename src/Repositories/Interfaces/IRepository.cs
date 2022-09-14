6ede04fbbc1b99f809a738e0bc6683173d8b984b
Bárdos István <bardos.istvan@icloud.com>
2022-09-14 16:29:39 +0200

    Add generic GetAll() method to IRepository.cs

--- src/Repositories/Interfaces/IRepository.cs
+++ src/Repositories/Interfaces/IRepository.cs
@@ -1,6 +1,10 @@
+using System.Collections.Generic;
+using HogwartsHouses.Models;
+
 namespace HogwartsHouses.Repositories.Interfaces
 {
     public interface IRepository<T>
     {
+        HashSet<T> GetAll();
     }
 }
