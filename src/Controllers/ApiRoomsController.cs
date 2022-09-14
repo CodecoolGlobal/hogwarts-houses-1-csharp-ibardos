7673fcc3252b24c615a0436fa2d02018083a5359
Bárdos István <bardos.istvan@icloud.com>
2022-09-14 16:37:34 +0200

    Create ApiRoomsController.cs

---
+++ src/Controllers/ApiRoomsController.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using HogwartsHouses.Models;
+using HogwartsHouses.Services;
+using HogwartsHouses.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HogwartsHouses.Controllers;
+
+[ApiController, Route("[controller]")]
+public class ApiRoomsController : ControllerBase
+{
+}
