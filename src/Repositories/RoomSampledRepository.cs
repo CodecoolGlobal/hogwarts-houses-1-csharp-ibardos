959d1c38745e4890a46467b21cbb1e13e7f83231
Bárdos István <bardos.istvan@icloud.com>
2022-09-15 12:18:01 +0200

    Remove unused import

--- src/Repositories/RoomSampledRepository.cs
+++ src/Repositories/RoomSampledRepository.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using HogwartsHouses.Models;
 using HogwartsHouses.Repositories.Interfaces;
 using HogwartsHouses.Services;
