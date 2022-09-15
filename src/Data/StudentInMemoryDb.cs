53ae68359156b09f545b923b7c812cb73744b34e
Bárdos István <bardos.istvan@icloud.com>
2022-09-15 17:11:26 +0200

    Implement Initialize() to create a starter inMemory DB

--- src/Data/StudentInMemoryDb.cs
+++ src/Data/StudentInMemoryDb.cs
@@ -14,5 +14,13 @@ namespace HogwartsHouses.Data
         {
             Initialize();
         }
+
+        public void Initialize()
+        {
+            Students = new HashSet<Student>();
+
+            AddStudent(new Student("Hermione Granger", PetType.Cat));
+            AddStudent(new Student("Draco Malfoy", PetType.None));
+        }
     }
 }
