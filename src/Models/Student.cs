d1fd5296fd79daae3466d177486085c4f7c6633f
Bárdos István <bardos.istvan@icloud.com>
2022-09-15 15:00:25 +0200

    Add Id property for Student type

--- src/Models/Student.cs
+++ src/Models/Student.cs
@@ -5,11 +5,15 @@ namespace HogwartsHouses.Models
     [System.Serializable]
     public class Student
     {
+        private static int _lastId;
+        public int Id { get; set; }
         public string Name { get; }
         public PetType? Pet { get; set; }
 
         public Student(string name, PetType? pet)
         {
+            Id = _lastId;
+            _lastId++;
             Name = name;
             Pet = pet;
         }
